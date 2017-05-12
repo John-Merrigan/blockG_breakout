e7650aeae65bef38665b932357bdf0d5ff036a26
John-Merrigan <jmerrigan18@stjohnsprep.org>
2017-05-12 10:54:32 -0400

    Add files via upload

---
+++ bounce.py
@@ -0,0 +1,38 @@
+from graphics import *
+
+# instantiate window
+win = GraphWin("window", 600, 600)
+
+# instantiate a point with (x, y) coordinates of (160, 120)
+center = Point(160, 120)
+
+# instantiate ball with center at (160, 120)
+ball = Circle(center, 20)
+
+# fill the circle with black
+ball.setFill("BLACK")
+
+# draw the circle to the window
+ball.draw(win)
+
+velocity = 2
+
+while True:
+
+    # move ball along x-axis
+    ball.move(velocity, 0)
+
+    # get x-coordinate of circle
+    centerBall = ball.getCenter()
+    xBall = centerBall.getX()
+
+    # bounce off right edge of window
+    if xBall + 20 > 320:
+        velocity = -velocity - 100
+
+    # bounce off left edge of window
+    elif xBall - 20 < 0:
+        velocity = -velocity
+
+
+exit(0)
